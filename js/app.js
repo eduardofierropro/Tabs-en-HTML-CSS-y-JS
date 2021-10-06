d573771aba4f3996589dca82f280dc935d943190
Eduardo Fierro Pro <91935331+eduardofierro-pro@users.noreply.github.com>
2021-10-06 23:42:09 +0200

    Añadiendo código base

---
+++ js/app.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const li        = document.querySelectorAll('.li')
+const bloque    = document.querySelectorAll('.bloque')
+
+// CLICK en li
+    // TODOS .li quitar la clase activo
+    // TODOS .bloque quitar la clase activo
+    // .li con la posicion se añadimos la clase activo
+    // .bloque con la posicion se añadimos la clase activo
+
+// Recorriendo todos los LI
+li.forEach( ( cadaLi , i )=>{
+    // Asignando un CLICK a CADALI
+    li[i].addEventListener('click',()=>{
+
+        // Recorrer TODOS los .li
+        li.forEach( ( cadaLi , i )=>{
+            // Quitando la clase activo de cada li
+            li[i].classList.remove('activo')
+            // Quitando la clase activo de cada bloque
+            bloque[i].classList.remove('activo')
+        })
+
+        // En el li que hemos click le añadimos la clase activo
+        li[i].classList.add('activo')
+        // En el bloque con la misma posición le añadimos la clase activo
+        bloque[i].classList.add('activo')
+
+    })
+})
